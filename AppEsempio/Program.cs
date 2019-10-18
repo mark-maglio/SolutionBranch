16824413c4ae183c41af91e614859ce703e142a2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-18 12:38:24 +0200

    Creazione ConsoleApp

---
+++ AppEsempio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEsempio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ciao");
+        }
+    }
+}
